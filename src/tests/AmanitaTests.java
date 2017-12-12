3f2450472a70462b7b971dcb9bfcf4c937a4fb78
InDevRus <InDevRus@hotmail.com>
2017-12-12 23:27:26 +0500

    Fixed amanita tests, added new.

--- src/tests/AmanitaTests.java
+++ src/tests/AmanitaTests.java
@@ -5,14 +5,23 @@ import control.Point;
 import logic.Amanita;
 import logic.Grid;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class AmanitaTests {
 
-    private Configuration config = new Configuration();
-    private Grid gridEx = new Grid(config);
-    private Grid.obj[][] grid = gridEx.getGrid();
-    private Amanita amanita = new Amanita(gridEx, config);
+    private Configuration config;
+    private Grid gridEx;
+    private Grid.obj[][] grid;
+    private Amanita amanita;
+
+    @BeforeMethod
+    public void initialization() {
+        config = new Configuration();
+        gridEx = new Grid(config);
+        grid = gridEx.getGrid();
+        amanita = new Amanita(gridEx, config);
+    }
 
     @Test
     public void updatePositionOfAmanitaPoisonedTrue() {
@@ -36,5 +45,33 @@ public class AmanitaTests {
         Point oldAmanita = amanita.getLocation();
         amanita.create();
         Assert.assertNotEquals(oldAmanita, amanita.getLocation());
+    }
+
+    @Test
+    public void updateWithNullLocation() {
+        amanita.setFreq(0);
+        amanita.setLocation(null);
+        amanita.updatePositionOfAmanita();
+        Point location = amanita.getLocation();
+        Assert.assertNotNull(location);
+        Assert.assertEquals(grid[location.getY()][location.getX()], Grid.obj.AMANITA);
+    }
+
+    @Test
+    public void updateWithNoLifetime() {
+        amanita.setFreq(0);
+        amanita.setLifeTime(0);
+        amanita.updatePositionOfAmanita();
+        Assert.assertNull(amanita.getLocation());
+        Assert.assertEquals(amanita.getLifeTime(), config.getLifeTimeAmanita());
+        Assert.assertEquals(amanita.getFreq(), config.getFrequencyOfOccurrenceAmanita());
+    }
+
+    @Test
+    public void decreaseLifetime() {
+        amanita.setFreq(0);
+        amanita.setLifeTime(123);
+        amanita.updatePositionOfAmanita();
+        Assert.assertEquals(amanita.getLifeTime(), 123 - 1);
     }
 }
